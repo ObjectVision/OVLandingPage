2c60f5a8b9a1aed72014cb62d1ae4ea2b804a9ec
eoudejans <eoudejans@objectvision.nl>
2024-04-14 23:37:29 +0200

    Implemented dynamic toggle of header template item: "Product".

--- landing_page/src/app/header/header.component.ts
+++ landing_page/src/app/header/header.component.ts
@@ -1,12 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
 
+  product_state : boolean = true;
+
+  onProductToggle() {
+    this.product_state = !this.product_state;
+    console.log("TOGGLE" + this.product_state);
+  }
 }
