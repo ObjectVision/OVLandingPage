15470af429efd6a8e7c894c9b604d278354107ac
eoudejans <eoudejans@objectvision.nl>
2024-04-16 14:21:26 +0200

    Added jumbotron component.

--- landing_page/src/app/app.component.ts
+++ landing_page/src/app/app.component.ts
@@ -1,14 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { initFlowbite } from 'flowbite';
+
 import { HeaderComponent } from './header/header.component';
+import { JumbotronComponent } from './jumbotron/jumbotron.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, JumbotronComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'landing_page';
+
+  ngOnInit(): void {
+    initFlowbite();
+  }
 }
